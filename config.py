102f9315dc27aabeb8407aca883d088217019942
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-04 16:07:20 +0300

    Edited Production files

--- config.py
+++ config.py
@@ -17,8 +17,10 @@ class Config:
 
 
 class ProdConfig(Config):
+
     pass
 
+    SQLALCHEMY_DATABASE_URI ='postgres://vfgysrcwsqfxal:ad7f3f1e5d3141e4b26d53b01268bd7e9afdadbba5682686aa18a394794e4db2@ec2-34-231-56-78.compute-1.amazonaws.com:5432/dbdd0icu7a7i5v'
 
 class DevConfig(Config):
     DEBUG = True
