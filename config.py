bc055644f37e5322308fdcc72a2f31d629daf577
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-05 09:11:52 +0300

    Final push and deployment

--- config.py
+++ config.py
@@ -17,10 +17,9 @@ class Config:
 
 
 class ProdConfig(Config):
-    pass
 
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogspot'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
